4887bb29cdebd88e9471ff66fdeb9cbb47de3b23
notdav04 <davide.alonzi26@gmail.com>
2024-12-10 15:56:05 +0100

    minor fixes

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,5 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import mainReducer from "../reducers";
-import removeFavReducer from "../reducers/removeFavReducer";
 
 const rootStore = combineReducers({
   main: mainReducer
